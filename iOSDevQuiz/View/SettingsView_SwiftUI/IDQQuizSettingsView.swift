ea2ca529de3ff5265e503468e12c7c4a53c9da3f
Daniel Karath <karath.dani@gmail.com>
2023-04-27 21:37:20 +0200

    Added a no questions found label to the disabled question menu

--- iOSDevQuiz/View/SettingsView_SwiftUI/IDQQuizSettingsView.swift
+++ iOSDevQuiz/View/SettingsView_SwiftUI/IDQQuizSettingsView.swift
@@ -19,52 +19,64 @@ struct IDQQuizSettingsView: View {
     var topPaddingModifier: CGFloat = UIScreen.screenHeight >= 700 ? 0.05 : 0.03
     
     
-    var body: some View {        
+    var body: some View {
         ZStack {
             Rectangle()
                 .fill(Color(IDQConstants.backgroundColor))
                 .ignoresSafeArea()
             VStack {
                 HStack {
+                    Text("Disabled Questions")
+                        .font(Font(IDQConstants.setFont(fontSize: 20, isBold: false)))
+                        .foregroundColor(Color(IDQConstants.basicFontColor))
+                        .padding(.leading, 16)
                     Spacer()
                     IDQDismissButtonView(isViewVisible: $isQuizSettingsVisible, color: Color(uiColor: IDQConstants.basicFontColor))
                 }
-                    .padding(.trailing, 16)
-                    .padding(.top, UIScreen.screenHeight * topPaddingModifier)
+                .padding(.trailing, 16)
+                .padding(.top, UIScreen.screenHeight * topPaddingModifier)
                 Spacer()
                 VStack {
                     if let questions = questionArray {
-                        List(Array(questions.indices), id: \.self) { index in
-                            Button(action: {
-                                selectedItemIndex = index
-                                isConfirmationAlertPresented = true
-                            }, label: {
-                                Text(questions[index].question ?? "")
-                                    .font(Font(IDQConstants.setFont(fontSize: 16, isBold: false)))
-                                    .foregroundColor(Color(IDQConstants.basicFontColor))
-                            })
-                        }
-                        .background(Color(IDQConstants.backgroundColor))
-                        .scrollContentBackground(.hidden)
-                        .alert(isPresented: $isConfirmationAlertPresented, content: {
-                            let question = questionArray?[selectedItemIndex ?? 0]
-                            return Alert(
-                                title: Text("Restore question"),
-                                message: Text("Do you want to restore the question:\n\n \"\(question?.question ?? "")\""),
-                                primaryButton: .default(Text("Yes")) {
-                                    print("Question restored")
-                                    if let question = questionArray?[selectedItemIndex ?? 0] {
-                                        viewModel.removeDisable(question)
-                                        questionArray = viewModel.fetchQuestionArray(for: .disabled)
+                        if !questions.isEmpty {
+                            List(Array(questions.indices), id: \.self) { index in
+                                Button(action: {
+                                    selectedItemIndex = index
+                                    isConfirmationAlertPresented = true
+                                }, label: {
+                                    Text(questions[index].question ?? "")
+                                        .font(Font(IDQConstants.setFont(fontSize: 16, isBold: false)))
+                                        .foregroundColor(Color(IDQConstants.basicFontColor))
+                                })
+                            }
+                            .background(Color(IDQConstants.backgroundColor))
+                            .scrollContentBackground(.hidden)
+                            .alert(isPresented: $isConfirmationAlertPresented, content: {
+                                let question = questionArray?[selectedItemIndex ?? 0]
+                                return Alert(
+                                    title: Text("Restore question"),
+                                    message: Text("Do you want to restore the question:\n\n \"\(question?.question ?? "")\""),
+                                    primaryButton: .default(Text("Yes")) {
+                                        print("Question restored")
+                                        if let question = questionArray?[selectedItemIndex ?? 0] {
+                                            viewModel.removeDisable(question)
+                                            questionArray = viewModel.fetchQuestionArray(for: .disabled)
+                                        }
+                                    },
+                                    secondaryButton: .cancel(Text("No")) {
+                                        print("Question left in disabled")
                                     }
-                                },
-                                secondaryButton: .cancel(Text("No")) {
-                                    print("Question left in disabled")
-                                }
-                            )
-                        })
+                                )
+                            })
+                        } else {
+                            Text("No questions found")
+                                .font(Font(IDQConstants.setFont(fontSize: 12, isBold: false)))
+                                .foregroundColor(Color(IDQConstants.basicFontColor))
+                        }
                     } else {
                         Text("No questions found")
+                            .font(Font(IDQConstants.setFont(fontSize: 12, isBold: false)))
+                            .foregroundColor(Color(IDQConstants.basicFontColor))
                     }
                 }
                 Spacer()
